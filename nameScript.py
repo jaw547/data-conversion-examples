feeced8e7c4c1123958f8bdf0d498a1aff45b535
Jackson Wright <jaw547@student.hks.harvard.edu>
2018-01-10 13:19:38 -0500

    added comments to code

--- nameScript.py
+++ nameScript.py
@@ -1,10 +1,14 @@
+#Read from name.txt
 with open('name.txt') as f:
 	my_name = f.read()
 
+#Create a new variable with the string you want.
 writeMe = 'Hello, my name is ' + my_name + '.'
 
+#Save the string to a new file.
 with open('hello.txt', 'w') as f:
 	f.write(writeMe)
 	f.write('\n') 
 
+#Print the variable to the console for ease.
 print writeMe
