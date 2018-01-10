23f8ca2230a057a02f717ff043ecd9d8439b03c2
Jackson Wright <jaw547@student.hks.harvard.edu>
2018-01-10 15:33:45 -0500

    add comments to code

--- cook.py
+++ cook.py
@@ -1,13 +1,12 @@
+#Import ur modules.
 import csv
+import json
 
+#Read a csv and turn it into JSON dict.
 with open('vegetables.csv') as f:
     reader = csv.DictReader(f)
     rows = list(reader)
 
-for row in rows:
-    print(row)
-
-import json
-
+#Write to JSON file.
 with open('veggies.json', 'w') as f:
     json.dump(rows, f, indent=2)
